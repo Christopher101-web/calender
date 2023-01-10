924659a69cd70e3c6462e499ff466c4fb6cbee70
Your Name <you@example.com>
2023-01-10 14:02:36 +0200

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const monthEl = document.querySelector('.date h1');
 const fullDateEl = document.querySelector('.date p');
 const daysEl = document.querySelector('.days');
 const monthInx = new Date().getMonth();
-const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate() + 1;
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1,0).getDate() +1;
 const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
 
 const months = [
@@ -27,7 +27,7 @@ for (let i = firstDay; i > 0; i--){
 	days += `<div class="empty"></div>`;
 }
 
-for (let i = 1; i < lastDay; i++){
+for (let i = 2; i < lastDay; i++){
 	if (i === new Date().getDate()){
 		days += `<div class="today">${i}</div>`;
 	} else {
